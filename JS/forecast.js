559f7fa187be7aa05bbff4112ad91b04d40ca836
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2021-12-17 21:10:43 +0300

    Add Weather and location API

--- JS/forecast.js
+++ JS/forecast.js
@@ -1,1 +1,32 @@
 //Todo use for forecast
+const key="	4NnxoTKabE7gE6UoVlYzsmR3pPpAbOS4";
+//get weather conditions of the specified API
+const getWeather = async(location_key)=>{
+    const base="http://dataservice.accuweather.com/currentconditions/v1/";
+    const query=`${location_key}?apikey=${key}`;
+    const response=await fetch (base+query);
+    const weather_condition=await response.json();
+    // console.log(weather_condition);
+    return weather_condition[0];
+}
+//location Api invocation
+const getKey= async(city)=>{
+    const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query=`?apikey=${key}&q=${city}`;
+    const response=await fetch(base+query);
+    const cityData=await response.json();
+
+    return cityData[0];
+
+}
+// getKey("Nairobi").then((data)=>{
+//         // console.log(data);
+//         return getWeather(data.Key);
+//     }).then((data)=>{
+//         console.log(data);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     });
+
+// // getWeather("224758")
--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,18 @@
 //Todo use for common JS Interaction
+const cityForm=document.querySelector("form");
+const updateCityDetails = async(city)=>{
+    const cityDet=await getKey(city);
+    const cityWeather=await getWeather(cityDet.Key);
+    return {cityDetails:cityDet,cityWeatherConditions:cityWeather};
+}
+cityForm.addEventListener("submit",e=>{
+    e.preventDefault();
+    const city= cityForm.city.value.trim();
+    cityForm.reset();
+    //update the UI
+    updateCityDetails(city).then((data)=>{
+        console.log(data);
+    }).catch((err)=>{
+        console.log(err);
+    });
+});
