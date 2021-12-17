7a61cf7a93f698c3a10d31ab00a47b011ef966e1
vicky-magiri <ndung'u.victor19@students.dkut.ac.ke>
2021-12-17 22:53:45 +0300

    Update UI

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,43 @@
 //Todo use for common JS Interaction
+
+//fetching the rquired elements of the UI
 const cityForm=document.querySelector("form");
+const card=document.querySelector(".city_card");
+const city_name=document.querySelector(".city_name");
+const CityWeather=document.querySelector(".weather_conditions");
+const temp=document.querySelector(".temp");
+
+//UI of time day weather condition
+const time=document.querySelector(".time");
+const icon=document.querySelector(".weather_icon img");
+
+const updateUI= (data)=>{
+    const cityDetails=data.cityDetails;
+    const weather=data.cityWeatherConditions;
+    const cityName=cityDetails.EnglishName;
+    const cityWeather=weather.WeatherText;
+    const cityTemperature=weather.Temperature.Metric.Value;
+    city_name.innerText=cityName;
+    CityWeather.innerText=cityWeather;
+    temp.innerText=cityTemperature;
+
+    //update the UI of the time of the day
+    let timeSrc=null;
+    if(weather.isDayTime){
+        timeSrc="img/day.svg";
+    }else{
+        timeSrc="img/night.svg";
+    }
+    time.setAttribute("src",timeSrc);
+
+    //adding the iconsrc
+    let iconSrc=`img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute("src",iconSrc);
+
+    card.style.display="block";
+    // const weather=
+
+}
 const updateCityDetails = async(city)=>{
     const cityDet=await getKey(city);
     const cityWeather=await getWeather(cityDet.Key);
@@ -12,6 +50,7 @@ cityForm.addEventListener("submit",e=>{
     //update the UI
     updateCityDetails(city).then((data)=>{
         console.log(data);
+        updateUI(data);
     }).catch((err)=>{
         console.log(err);
     });
